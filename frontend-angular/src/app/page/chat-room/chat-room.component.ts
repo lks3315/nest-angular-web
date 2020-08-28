8d62371f5ebf637f177af7bda3f41a8792899ec9
KSLee <pow3351@tvstorm.com>
2020-08-28 18:54:15 +0900

    2nd commit

--- frontend-angular/src/app/page/chat-room/chat-room.component.ts
+++ frontend-angular/src/app/page/chat-room/chat-room.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MaxLength} from "class-validator";
+import {Trim} from "class-sanitizer";
+import {ActivatedRoute} from "@angular/router";
+import {Socket} from "ngx-socket-io";
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.css']
 })
-export class ChatRoomComponent implements OnInit {
+export class ChatRoomComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  messages = [];
+
+  @MaxLength(40, {message: '$constraint1보다 작아야 합니다.'})
+  @Trim()
+  nickname = '';
+
+  @MaxLength(200, {message: '$constraint1보다 작아야 합니다.'})
+  @Trim()
+  message = '';
+
+  joinMsg = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private socket: Socket
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.nickname = params.nickname;
+    });
+
+    this.socket.on('message', message => this.messages.push(message));
+
+    this.socket.on('users-changed', (data) => {
+      const user = data['user'];
+      if (data['event'] === 'left') {
+        this.joinMsg = `${user}님이 나가셨습니다.`;
+      } else {
+        this.joinMsg = `${user}님이 참가했습니다.`;
+      }
+    });
   }
 
+  sendMessage() {
+    this.socket.emit('add-message', {text: this.message})
+    this.message = '';
+  }
+
+  ngOnDestroy(): void {
+    this.socket.disconnect();
+  }
 }
--- frontend-angular/src/app/page/chat-room/chat-room.module.ts
+++ frontend-angular/src/app/page/chat-room/chat-room.module.ts
@@ -3,13 +3,17 @@ import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {ChatRoomComponent} from "./chat-room.component";
 import {ChatRoomRoutingModule} from "./chat-room-routing.module";
+import {MatTableModule} from "@angular/material/table";
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ChatRoomRoutingModule
+    ChatRoomRoutingModule,
+    MatTableModule,
+    MatCardModule
   ],
   declarations: [ChatRoomComponent]
 })
